97c5771d960e6a910780669db1767542e3d420f3
kibnm821 <kibnm821@naver.com>
2019-08-19 11:12:19 +0900

    Spring

--- src/main/java/com/example/groom_spring/Controller/example.java
+++ src/main/java/com/example/groom_spring/Controller/example.java
@@ -1,8 +1,5 @@
 package com.example.groom_spring.Controller;
 
-import com.example.groom_spring.service.ExampleService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +8,9 @@ import org.springframework.web.client.RestTemplate;
 @CrossOrigin
 @RestController
 public class Example{
-    @Autowired
-    ExampleService exampleService;
     @RequestMapping(value = "/weather")
     public Object actionMethod(){
         RestTemplate restTemplate = new RestTemplate();
-        Object tempObject = new Object();
         String keyId = "35401e6e815d96876657f9a20945be7a";
         String targetUri = "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid="+keyId;
         Object resultObject = restTemplate.getForObject(targetUri, Object.class);
