5d6f4b0475ddf8b699df617ce601d89a8b695271
kibnm821 <kibnm821@naver.com>
2019-08-19 13:22:05 +0900

    Spring

--- src/main/java/com/example/groom_spring/Controller/example.java
+++ src/main/java/com/example/groom_spring/Controller/example.java
@@ -1,18 +1,25 @@
 package com.example.groom_spring.Controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin
 @RestController
+
 public class Example{
-    @RequestMapping(value = "/weather")
-    public Object actionMethod(){
+    @RequestMapping(value = "/weather", method = RequestMethod.GET)
+    public Object actionMethod(@RequestParam Map<String,Object> paramMap){
+
         RestTemplate restTemplate = new RestTemplate();
+
         String keyId = "35401e6e815d96876657f9a20945be7a";
-        String targetUri = "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid="+keyId;
+        String targetUri = "http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid="+keyId;
         Object resultObject = restTemplate.getForObject(targetUri, Object.class);
         return resultObject;
     }
